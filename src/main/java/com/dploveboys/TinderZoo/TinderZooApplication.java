40af2eb53701aabb5102928b3d246d3becd82e4b
Caracoancea Timotei <caracoancea.timotei@gmail.com>
2021-05-05 14:06:38 +0300

    Initial commit

---
+++ src/main/java/com/dploveboys/TinderZoo/TinderZooApplication.java
@@ -0,0 +1,13 @@
+package com.dploveboys.TinderZoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderZooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinderZooApplication.class, args);
+	}
+
+}
