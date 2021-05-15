39e0999b3bf723395bd285dc212bd8979ae603dc
Caracoancea Timotei <caracoancea.timotei@gmail.com>
2021-05-15 13:34:36 +0300

    feature/TIN-9 Added sql script

--- src/main/scripts/config_database.sql
+++ src/main/scripts/config_database.sql
@@ -32,7 +32,7 @@ CREATE TABLE `users` (
 
 CREATE TABLE `profile_pictures` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
+  `user_id` int UNIQUE NOT NULL,
   `image` longblob NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
